1b75beb5ef64181423d419de89f7358d6a00a822
or menashe <menesh2@gmail.com>
2018-03-20 00:26:12 +0200

    increased images size

--- gefen/Flicker/FlickerManager.swift
+++ gefen/Flicker/FlickerManager.swift
@@ -15,7 +15,6 @@ protocol FlickerManagerDelegate: class {
 
 class FlickerManager{
     static let ImagesUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d86308194ea517acd62ad69af14025ac&page=1&format=json&nojsoncallback=1&auth_token=72157664842358407-6b16bbea87dc3710&api_sig=523113ee5635726911c6d62a7cbd42c0"
-    static let photoUrlpattern = "https:\\\\farm{farm}.staticflickr.com\\{server}\\{id}_secret_m.jpg"
     
     static func sendPhotosRequest(completionHandler:@escaping ([ImageData])->()) {
         
@@ -53,7 +52,7 @@ class FlickerManager{
     static func getPhoto(imageData:ImageData, completionCallback:@escaping (ImageData)->() ) {
         
         
-        let photoUrl = "https://farm\(imageData.farm).staticflickr.com/\(imageData.server)/\(imageData.imageId)_\(imageData.secret)_m.jpg"
+        let photoUrl = "https://farm\(imageData.farm).staticflickr.com/\(imageData.server)/\(imageData.imageId)_\(imageData.secret)_b.jpg"
         
         Alamofire.request(photoUrl).responseData(completionHandler: { (response) in
             print(response)
