8a2e19ef4728fda1ebf2ad0a5af04ee7c6e8feba
or menashe <menesh2@gmail.com>
2018-03-20 00:11:11 +0200

    added animation to loginScreen

--- gefen/MainViewController.swift
+++ gefen/MainViewController.swift
@@ -11,17 +11,20 @@ import Cartography
 
 class MainViewController: UIViewController {
     
-
+    let imageView: UIImageView
     let loginView: LoginView
     let loginLogic: LoginLogic
     
     init() {
+        imageView = UIImageView()
         loginView = LoginView()
         loginLogic = LoginLogic()
         super.init(nibName: nil, bundle: nil)
         loginLogic.delegate = self
         loginView.delegate = self
         view.addSubview(loginView)
+        view.addSubview(imageView)
+        initViews()
         setupConstraints()
         view.translatesAutoresizingMaskIntoConstraints = true
         view.backgroundColor = UIColor.white
@@ -31,13 +34,36 @@ class MainViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func initViews() {
+        imageView.alpha = 0
+        imageView.image = UIImage(named: "gefenLogin")
+    }
+    
     private func setupConstraints() {
         constrain(loginView) {loginView in
             guard let superView = loginView.superview else {return}
             loginView.left == superView.left + 40
             loginView.right == superView.right - 40
-            loginView.centerY == superView.centerY
+            loginView.centerY == superView.centerY + view.frame.size.height/2 + 40
+        }
+        
+        constrain(imageView, view){imageView, superView in
+            imageView.centerX == superView.centerX
+            imageView.top == superView.top + 100
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0, animations: {
+            constrain(self.loginView, self.view){loginView, superView in
+                loginView.centerY == superView.centerY
+            }
+            self.view.layoutIfNeeded()
+        }) { error in
             
+            UIView.animate(withDuration: 1.0, animations: {
+                self.imageView.alpha = 1
+            })
         }
     }
 
